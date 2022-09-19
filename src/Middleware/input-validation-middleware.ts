53f8f39a84c97089128bd9ab791a43d31d5198f5
ruslan2312 <heeca@mail.ru>
2022-09-19 15:20:07 +0300

    Validation

--- src/Middleware/input-validation-middleware.ts
+++ src/Middleware/input-validation-middleware.ts
@@ -1,17 +1,21 @@
 import {Request, Response, NextFunction} from "express";
 import {validationResult, ValidationError} from "express-validator";
 
+
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-
-    const errorFormatter = ({param}: ValidationError) => {
-        return {message: "Any<String>", filed: param};
-    };
-
-    const errors = validationResult(req).formatWith(errorFormatter);
-
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        res.status(400).json({errorsMessages: (errors.array())});
+        const err = errors.array().map(e => {
+            return {
+                message: e.msg,
+                field: e.param
+            }
+        })
+        res.status(400).json({errorsMessages: err})
     } else {
         next()
     }
 }
+
+
+// {onlyFirstError: true} read this option
